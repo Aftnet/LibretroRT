585575600a6cff3c9d60a35cb850d54c43200a7f
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-04-25 14:21:50 +0900

    Created winrt component, unit test app

---
+++ GPGC-RT.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GPGC-RT.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GPGC-RT.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ GPGX-RT/Class1.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace GPGX_RT
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+
+		Platform::String^ GetVersion();
+    };
+}
