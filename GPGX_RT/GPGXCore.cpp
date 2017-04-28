03fdfe4cf743d8b9cf2de043f85d9fcc974b12f3
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-04-28 17:00:42 +0900

    Logic

---
+++ GPGX_RT/GPGXCore.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "GPGXCore.h"
+
+#include "../GPGX/src/libretro/libretro.h"
+
+using namespace Platform;
+
+namespace GPGX_RT
+{
+	GPGXCore^ GPGXCore::instance = nullptr;
+
+	GPGXCore::GPGXCore()
+	{
+		retro_init();
+	}
+
+	GPGXCore::~GPGXCore()
+	{
+		if (instance != nullptr)
+		{
+			retro_deinit();
+		}
+		instance = nullptr;
+	}
+
+	void GPGXCore::StartExecution(unsigned long long dataSize)
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void GPGXCore::EndExecution()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+
+	void GPGXCore::Reset()
+	{
+		throw ref new Platform::NotImplementedException();
+	}
+}
---
+++ GPGX_RT/GPGXCore.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+using namespace LibRetroRT;
+
+namespace GPGX_RT
+{
+	public ref class GPGXCore sealed : ILibRetroCore
+    {
+    public:
+		static property GPGXCore^ Instance
+		{
+			GPGXCore^ get()
+			{
+				if (instance == nullptr)
+				{
+					instance = ref new GPGXCore();
+				}
+				return instance;
+			}
+		}
+
+		virtual property Platform::String ^ Name
+		{
+			Platform::String^ get() { return name; }
+		}
+
+		virtual property Platform::String ^ Version
+		{
+			Platform::String^ get() { return version; }
+		}
+
+		virtual property Platform::String ^ SupportedExtensions
+		{
+			Platform::String^ get() { return supportedExtensions; }
+		}
+
+		virtual property LibRetroRT::AV::SystemAVInfo AVInfo
+		{
+			LibRetroRT::AV::SystemAVInfo get() { return avInfo; }
+		}
+
+		virtual ~GPGXCore();
+
+		virtual event LibRetroRT::ReadDataDelegate ^ ReadData;
+		virtual event LibRetroRT::RenderVideoFrameDelegate ^ RenderVideoFrame;
+		virtual event LibRetroRT::RenderAudioFramesDelegate ^ RenderAudioFrames;
+		virtual void StartExecution(unsigned long long dataSize);
+		virtual void EndExecution();
+		virtual void Reset();
+
+	private:
+		static GPGXCore^ instance;
+
+		Platform::String^ name;
+		Platform::String^ version;
+		Platform::String^ supportedExtensions;
+		LibRetroRT::AV::SystemAVInfo avInfo;
+
+		GPGXCore();
+	};
+}
