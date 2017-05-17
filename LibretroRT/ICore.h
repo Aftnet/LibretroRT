44568d2d75e24d9a25cf1bc633ff516875d8d028
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-05-17 19:02:20 +0900

    Logic

---
+++ LibretroRT/ICore.h
@@ -0,0 +1,37 @@
+#pragma once
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Storage::Streams;
+
+namespace LibretroRT
+{
+	public delegate void RenderVideoFrameDelegate(const Array<uint8>^ frameBuffer, uint32 width, uint32 height, uint32 pitch);
+	public delegate void RenderAudioFramesDelegate(const Array<int16>^ data, uint32 numFrames);
+	
+	public delegate void PollInputDelegate();
+	public delegate int16 GetInputStateDelegate();
+
+	public interface class ICore
+	{
+	public:
+		property String^ Name { String^ get(); };
+		property String^ Version { String^ get(); };
+		property String^ SupportedExtensions { String^ get(); };
+
+		void LoadGame(IRandomAccessStream^ gameStream);
+		void UnloadGame();
+
+		void RunFrame();
+		void Reset();
+
+		event RenderVideoFrameDelegate^ RenderVideoFrame;
+		event RenderAudioFramesDelegate^ RenderAudioFrames;
+
+		event PollInputDelegate^ PollInput;
+		event GetInputStateDelegate^ GetInputState;
+	};
+}
+
+
