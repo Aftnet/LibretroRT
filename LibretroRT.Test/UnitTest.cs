a12e6966a462a10f9a9ae64705771d7c31f56889
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-05-18 17:26:24 +0900

    Logic

---
+++ LibretroRT.Test/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace LibretroRT.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CoreInfoIsRead()
+        {
+            var core = new GPGXRT.GPGXCore();
+
+            Assert.NotNull(core.Name);
+            Assert.NotNull(core.Version);
+            Assert.NotNull(core.SupportedExtensions);
+        }
+    }
+}
