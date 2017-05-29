d985f65e683f83179bf76f7f11248790652e2ccd
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-05-29 15:22:26 +0900

    Bugfixes

--- Snes9XRT/Snes9XCoreInternal.cpp
+++ Snes9XRT/Snes9XCoreInternal.cpp
@@ -19,26 +19,24 @@ Snes9XCoreInternal^ Snes9XCoreInternal::Instance::get()
 	if (coreInstance == nullptr)
 	{
 		coreInstance = ref new Snes9XCoreInternal();
+
+		retro_set_environment([](unsigned cmd, void* data) { return coreInstance->EnvironmentHandler(cmd, data); });
+		retro_set_input_poll([]() { coreInstance->RaisePollInput(); });
+		retro_set_input_state([](unsigned port, unsigned device, unsigned index, unsigned keyId) { return coreInstance->RaiseGetInputState(port, device, index, keyId); });
+		retro_set_audio_sample([](int16_t left, int16_t right) { coreInstance->SingleAudioFrameHandler(left, right); });
+		retro_set_audio_sample_batch([](const int16_t* data, size_t numFrames) { return coreInstance->RaiseRenderAudioFrames(data, numFrames); });
+		retro_set_video_refresh([](const void *data, unsigned width, unsigned height, size_t pitch) { coreInstance->RaiseRenderVideoFrame(data, width, height, pitch); });
+
+		retro_init();
 	}
 	return coreInstance;
 }
 
 Snes9XCoreInternal::Snes9XCoreInternal()
 {
-	pixelFormat = PixelFormats::FormatRGB565;
-
 	retro_system_info info;
 	retro_get_system_info(&info);
 	SetSystemInfo(info);
-
-	retro_set_environment([](unsigned cmd, void* data) { return coreInstance->EnvironmentHandler(cmd, data); });
-	retro_set_input_poll([]() { coreInstance->RaisePollInput(); });
-	retro_set_input_state([](unsigned port, unsigned device, unsigned index, unsigned keyId) { return coreInstance->RaiseGetInputState(port, device, index, keyId); });
-	retro_set_audio_sample([](int16_t left, int16_t right) { coreInstance->SingleAudioFrameHandler(left, right); });
-	retro_set_audio_sample_batch([](const int16_t* data, size_t numFrames) { return coreInstance->RaiseRenderAudioFrames(data, numFrames); });
-	retro_set_video_refresh([](const void *data, unsigned width, unsigned height, size_t pitch) { coreInstance->RaiseRenderVideoFrame(data, width, height, pitch); });
-
-	retro_init();
 }
 
 Snes9XCoreInternal::~Snes9XCoreInternal()
