3f026e8084a92e8b199e6a561d41e35c3d0404b7
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-06-06 11:26:42 +0900

    Logic

---
+++ LibretroRT.FrontendComponents.Common/ICoreRunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage;
+
+namespace LibretroRT.FrontendComponents.Common
+{
+    public interface ICoreRunner
+    {
+        void LoadGame(ICore core, IStorageFile gameFile);
+        void UnloadGame();
+
+        void PauseGame();
+        void ResumeGame();
+        void ResetGame();
+
+        bool LoadState(IStorageFile stateFile);
+        bool SaveState(IStorageFile stateFile);
+    }
+}
