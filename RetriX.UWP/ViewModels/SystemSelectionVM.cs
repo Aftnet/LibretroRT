4eb048d50343f512344b896061151e88ef54759f
Alberto Fustinoni <alberto@aftnet.net>
2017-06-07 08:50:43 +0900

    Logic

---
+++ RetriX.UWP/ViewModels/SystemSelectionVM.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetriX.UWP.ViewModels
+{
+    public class SystemSelectionVM : ViewModelBase
+    {
+    }
+}
