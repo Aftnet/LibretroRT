15a4376ff3b2fd8dc600ea4e4b75040b05b7141f
Alberto Fustinoni <alberto@aftnet.net>
2017-06-07 22:09:59 +0900

    Logic

---
+++ LibretroRT.FrontendComponents.MonoGameRenderer/MonoGameRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibretroRT.FrontendComponents.MonoGameRenderer
+{
+    public sealed class MonoGameRenderer
+    {
+    }
+}
