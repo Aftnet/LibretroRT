b1868fc84301a7f096ebda265fb4c29000ee8908
Alberto Fustinoni <alberto@aftnet.net>
2017-06-09 01:26:20 +0900

    logic

--- RetriX.Shared/ViewModels/GamePlayerVM.cs
+++ RetriX.Shared/ViewModels/GamePlayerVM.cs
@@ -1,8 +1,35 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using RetriX.Shared.Services;
 
 namespace RetriX.Shared.ViewModels
 {
     public class GamePlayerVM : ViewModelBase
     {
+        private readonly IEmulationService EmulationService;
+
+        public RelayCommand ToggleFullScreen { get; private set; }
+        public bool IsFullScreenMode => EmulationService.IsFullScreenMode;
+
+        public GamePlayerVM(IEmulationService emulationService)
+        {
+            EmulationService = emulationService;
+
+            ToggleFullScreen = new RelayCommand(ToggleFullScreenHandler);
+        }
+
+        private void ToggleFullScreenHandler()
+        {
+            if (EmulationService.IsFullScreenMode)
+            {
+                EmulationService.ExitFullScreen();
+            }
+            else
+            {
+                EmulationService.TryEnterFullScreen();
+            }
+
+            RaisePropertyChanged(nameof(IsFullScreenMode));
+        }
     }
 }
--- RetriX.UWP/Services/EmulationService.cs
+++ RetriX.UWP/Services/EmulationService.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -27,7 +28,9 @@ namespace RetriX.UWP.Services
         };
 
         public ICoreRunner CoreRunner { get; set; }
+        public bool IsFullScreenMode => AppView.IsFullScreenMode;
 
+        private ApplicationView AppView => ApplicationView.GetForCurrentView();
         private Frame RootFrame => Window.Current.Content as Frame;
 
         public async void SelectAndRunGame(GameSystemTypes systemType)
@@ -39,6 +42,16 @@ namespace RetriX.UWP.Services
 
             RootFrame.Navigate(typeof(GamePlayerPage));
             var task = Task.Run(() => CoreRunner.LoadGame(core, file));
+        }
+
+        public bool TryEnterFullScreen()
+        {
+            return AppView.TryEnterFullScreenMode();
+        }
+
+        public void ExitFullScreen()
+        {
+            AppView.ExitFullScreenMode();
         }
 
         private Task<StorageFile> PickCoreSupportedGameFile(ICore core)
