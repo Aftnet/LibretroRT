4038f778d7cbb45ded563362f27fc60ac2ba74d0
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-06-12 14:32:29 +0900

    Logic

--- RetriX.UWP/Controls/PlayerOverlay.xaml.cs
+++ RetriX.UWP/Controls/PlayerOverlay.xaml.cs
@@ -8,14 +8,14 @@ namespace RetriX.UWP.Controls
 {
     public sealed partial class PlayerOverlay : UserControl
     {
-        public GamePlayerVM VM
+        public GamePlayerVM ViewModel
         {
             get { return (GamePlayerVM)GetValue(VMProperty); }
             set { SetValue(VMProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for VM.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty VMProperty = DependencyProperty.Register(nameof(VM), typeof(GamePlayerVM), typeof(PlayerOverlay), new PropertyMetadata(null));
+        public static readonly DependencyProperty VMProperty = DependencyProperty.Register(nameof(ViewModel), typeof(GamePlayerVM), typeof(PlayerOverlay), new PropertyMetadata(null));
 
 
         public PlayerOverlay()
