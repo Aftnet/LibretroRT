b331d99c8004e36f04a698532fcf61a4ea75a56b
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-06-13 17:59:52 +0900

    Tests

--- RetriX.Shared.Test/Services/SaveStateServiceTest.cs
+++ RetriX.Shared.Test/Services/SaveStateServiceTest.cs
@@ -10,7 +10,7 @@ namespace RetriX.Shared.Test.Services
         private const int InitializationDelayMs = 50;
 
         private const string GameId = nameof(GameId);
-        private const int SlotID = 4;
+        private const uint SlotID = 4;
 
         static readonly byte[] TestSavePayload = Enumerable.Range(0, byte.MaxValue).Select(d => (byte)d).ToArray();
 
@@ -42,7 +42,7 @@ namespace RetriX.Shared.Test.Services
         }
 
         [Fact]
-        public async Task SavingWorks()
+        public async Task SaveLoadDeleteWorks()
         {
             await Task.Delay(InitializationDelayMs);
 
@@ -57,6 +57,54 @@ namespace RetriX.Shared.Test.Services
 
             loaded = await Target.LoadStateAsync(SlotID);
             Assert.Equal(loaded, TestSavePayload);
+
+            await Target.ClearSavesAsync();
+
+            result = await Target.SlotHasDataAsync(SlotID);
+            Assert.False(result);
+
+            loaded = await Target.LoadStateAsync(SlotID);
+            Assert.Null(loaded);
+        }
+
+        [Fact]
+        public async Task DifferentSlotsAreIndependent()
+        {
+            await Task.Delay(InitializationDelayMs);
+
+            var result = await Target.SaveStateAsync(SlotID, TestSavePayload);
+            Assert.True(result);
+
+            var otherSlotID = SlotID + 1;
+
+            result = await Target.SlotHasDataAsync(otherSlotID);
+            Assert.False(result);
+
+            var loaded = await Target.LoadStateAsync(otherSlotID);
+            Assert.Null(loaded);
+
+            await Target.ClearSavesAsync();
+        }
+
+        [Fact]
+        public async Task ConcurrentOperationsAreBlocked()
+        {
+            var otherSlotID = SlotID + 1;
+
+            var result = await Target.SaveStateAsync(SlotID, TestSavePayload);
+            Assert.True(result);
+            result = await Target.SaveStateAsync(otherSlotID, TestSavePayload);
+            Assert.True(result);
+
+            var loadTasks = new Task<byte[]>[]
+            {
+                Target.LoadStateAsync(SlotID),
+                Target.LoadStateAsync(otherSlotID)
+            };
+
+            await Task.WhenAll(loadTasks);
+            Assert.NotNull(loadTasks[0].Result);
+            Assert.Null(loadTasks[1].Result);
 
             await Target.ClearSavesAsync();
         }
