12919a5f244d976beb6420c5b7e11e5decdac4d4
Alberto Fustinoni <alberto@aftnet.net>
2017-06-15 01:28:51 +0900

    Layout

--- RetriX.UWP/AppResources.xaml.cs
+++ RetriX.UWP/AppResources.xaml.cs
@@ -1,10 +1,27 @@
-﻿namespace RetriX.UWP
+﻿using NavigationMenuUWP;
+using RetriX.UWP.Pages;
+using Windows.ApplicationModel.Resources;
+
+namespace RetriX.UWP
 {
     partial class AppResources
     {
         public AppResources()
         {
             InitializeComponent();
+
+            var resLoader = ResourceLoader.GetForViewIndependentUse();
+
+            this.Add("TopNavItems", new NavMenuItem[]
+            {
+                new NavMenuItem {Symbol = "\ue7fc", Label = resLoader.GetString("GameSystemsNavItem\\Text"), DestPage = typeof(SystemSelectionPage) },
+            });
+
+            this.Add("BottomNavItems", new NavMenuItem[]
+            {
+                new NavMenuItem {Symbol = "\ue946", Label = resLoader.GetString("AboutNavItem\\Text"), DestPage = typeof(SystemSelectionPage) }
+            });
+
         }
     }
 }
