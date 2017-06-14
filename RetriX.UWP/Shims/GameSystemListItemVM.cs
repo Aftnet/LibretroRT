0efc1041573006f5d26a72b0b913d18282eec13f
Alberto Fustinoni <alberto@aftnet.net>
2017-06-15 00:57:24 +0900

    Layout

---
+++ RetriX.UWP/Shims/GameSystemListItemVM.cs
@@ -0,0 +1,12 @@
+﻿using RetriX.Shared.Services;
+
+namespace RetriX.UWP.ViewModels
+{
+    public class GameSystemListItemVM : Shared.ViewModels.GameSystemListItemVM
+    {
+        public GameSystemListItemVM(ILocalizationService localizer, GameSystemTypes type, string nameResKey, string manufacturerResKey, string symbol)
+            :base(localizer, type, nameResKey, manufacturerResKey, symbol)
+        {
+        }
+    }
+}
