c9df881a1c7f7d9f3fa6553fce9303cf54b7a632
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-07-03 14:07:44 +0900

    Logic

--- LibretroRT.FrontendComponents.Renderer/RenderTargetManager.cpp
+++ LibretroRT.FrontendComponents.Renderer/RenderTargetManager.cpp
@@ -26,6 +26,7 @@ RenderTargetManager::~RenderTargetManager()
 
 void RenderTargetManager::UpdateFormat(GameGeometry^ geometry, PixelFormats pixelFormat)
 {
+	RenderTargetAspectRatio = geometry->AspectRatio;
 	auto maxDimension = max(geometry->MaxWidth, geometry->MaxHeight);
 	auto requestedFormat = LibretroToDXGITextureFormatsMapping.find(pixelFormat)->second;
 
@@ -45,17 +46,32 @@ void RenderTargetManager::UpdateFormat(GameGeometry^ geometry, PixelFormats pixe
 	}
 }
 
-void UpdateRenderTarget()
-{
-
-}
-
 void RenderTargetManager::UpdateFromCoreOutput(const Array<byte>^ frameBuffer, unsigned int width, unsigned int height, unsigned int pitch)
 {
 }
 
 void RenderTargetManager::Render(CanvasDrawingSession^ drawingSession, Size canvasSize)
 {
+}
+
+ComPtr<ID3D11Texture2D> RenderTargetManager::CreateD3DTexture(ComPtr<ID3D11Device> device, DXGI_FORMAT format, unsigned int width, unsigned int height)
+{
+	D3D11_TEXTURE2D_DESC texDesc = { 0 };
+	texDesc.Width = width;
+	texDesc.Height = height;
+	texDesc.Format = format;
+	texDesc.MipLevels = 1;
+	texDesc.ArraySize = 1;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+	texDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
+
+	ComPtr<ID3D11Texture2D> output;
+	__abi_ThrowIfFailed(device->CreateTexture2D(&texDesc, nullptr, output.GetAddressOf()));
+	return output;
 }
 
 Rect RenderTargetManager::ComputeBestFittingSize(Size viewportSize, float aspectRatio)
@@ -84,26 +100,5 @@ unsigned int RenderTargetManager::ClosestGreaterPowerTwo(unsigned int value)
 		output *= 2;
 	}
 
-	return output;
-}
-
-
-ComPtr<ID3D11Texture2D> RenderTargetManager::CreateD3DTexture(ComPtr<ID3D11Device> device, DXGI_FORMAT format, unsigned int width, unsigned int height)
-{
-	D3D11_TEXTURE2D_DESC texDesc = { 0 };
-	texDesc.Width = width;
-	texDesc.Height = height;
-	texDesc.Format = format;
-	texDesc.MipLevels = 1;
-	texDesc.ArraySize = 1;
-	texDesc.SampleDesc.Count = 1;
-	texDesc.SampleDesc.Quality = 0;
-	texDesc.Usage = D3D11_USAGE_DEFAULT;
-	texDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-	texDesc.CPUAccessFlags = 0;
-	texDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;
-
-	ComPtr<ID3D11Texture2D> output;
-	__abi_ThrowIfFailed(device->CreateTexture2D(&texDesc, nullptr, output.GetAddressOf()));
 	return output;
 }
