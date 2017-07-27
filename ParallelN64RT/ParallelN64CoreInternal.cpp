316969242457f383def0d061b3f629784b4ffed8
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-07-27 17:41:07 +0900

    WinRT component

---
+++ ParallelN64RT/ParallelN64CoreInternal.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "ParallelN64CoreInternal.h"
+
+#include "../LibretroRT/libretro.h"
+#include "../LibretroRT_Tools/Converter.h"
+
+using namespace ParallelN64RT;
+using namespace LibretroRT_Tools;
+
+ParallelN64CoreInternal^ coreInstance = nullptr;
+
+ParallelN64CoreInternal^ ParallelN64CoreInternal::Instance::get()
+{
+	if (coreInstance == nullptr)
+	{
+		coreInstance = ref new ParallelN64CoreInternal();
+
+		retro_set_environment([](unsigned cmd, void* data) { return coreInstance->EnvironmentHandler(cmd, data); });
+		retro_set_input_poll([]() { coreInstance->RaisePollInput(); });
+		retro_set_input_state([](unsigned port, unsigned device, unsigned index, unsigned keyId) { return coreInstance->RaiseGetInputState(port, device, index, keyId); });
+		retro_set_audio_sample([](int16_t left, int16_t right) { coreInstance->SingleAudioFrameHandler(left, right); });
+		retro_set_audio_sample_batch([](const int16_t* data, size_t numFrames) { return coreInstance->RaiseRenderAudioFrames(data, numFrames); });
+		retro_set_video_refresh([](const void *data, unsigned width, unsigned height, size_t pitch) { coreInstance->RaiseRenderVideoFrame(data, width, height, pitch); });
+		retro_init();
+	}
+
+	return coreInstance;
+}
+
+ParallelN64CoreInternal::ParallelN64CoreInternal() : LibretroRT_Tools::CoreBase(retro_get_system_info, retro_get_system_av_info,
+	retro_load_game, retro_unload_game, retro_run, retro_reset, retro_serialize_size, retro_serialize, retro_unserialize, retro_deinit,
+	false, false)
+{
+	fileDependencies->Append(ref new FileDependency(L"disksys.rom", L"Famicom Disk System BIOS", L"ca30b50f880eb660a320674ed365ef7a"));
+}
+
+ParallelN64CoreInternal::~ParallelN64CoreInternal()
+{
+	coreInstance = nullptr;
+}
