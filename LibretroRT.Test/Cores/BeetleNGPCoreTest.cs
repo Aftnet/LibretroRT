a81878df187502c56493561982cf43fc11da219d
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-07-28 15:20:40 +0900

    Tests

---
+++ LibretroRT.Test/Cores/BeetleNGPCoreTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace LibretroRT.Test.Cores
+{
+    public class BeetleNGPCoreTest : TestBase
+    {
+        protected const string RomName = "Mr Driller.wsc";
+
+        public BeetleNGPCoreTest() : base(() => BeetleNGPRT.BeetleNGPCore.Instance)
+        {
+        }
+
+        [Theory]
+        [InlineData(RomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
+
+        [Theory]
+        [InlineData(RomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
+}
