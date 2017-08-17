105c83e46e203e4aa7ff7ce50f648772635efb4b
Alberto Fustinoni <alberto@aftnet.net>
2017-08-17 21:49:25 +0900

    Fixed PC engine CD

--- LibretroRT_Tools/file_path.cpp
+++ LibretroRT_Tools/file_path.cpp
@@ -5,11 +5,6 @@
 using namespace Platform;
 using namespace Windows::Storage;
 
-bool path_is_directory_internal(const char *path)
-{
-	throw ref new Platform::NotImplementedException("path_is_directory_internal");
-}
-
 bool path_is_character_special_internal(const char *path)
 {
 	return false;
@@ -38,17 +33,7 @@ int32_t path_get_size_internal(const char *path)
 	return output;
 }
 
-bool mkdir_norecurse_internal(const char *dir)
-{
-	throw ref new Platform::NotImplementedException("mkdir_norecurse_internal");
-}
-
 extern "C" {
-
-	bool path_is_directory(const char *path)
-	{
-		return path_is_directory_internal(path);
-	}
 
 	bool path_is_character_special(const char *path)
 	{
@@ -63,10 +48,5 @@ extern "C" {
 	int32_t path_get_size(const char *path)
 	{
 		return path_get_size_internal(path);
-	}
-
-	bool mkdir_norecurse(const char *dir)
-	{
-		return mkdir_norecurse_internal(dir);
 	}
 }
