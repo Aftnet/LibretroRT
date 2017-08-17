60b82c1f6661e3431a22a55cde80195cddf40914
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-08-17 11:31:01 +0900

    Logic

--- RetriX.UWP/ViewModels/GameSystemVM.cs
+++ RetriX.UWP/ViewModels/GameSystemVM.cs
@@ -11,12 +11,16 @@ namespace RetriX.UWP.ViewModels
 {
     public class GameSystemVM : RetriX.Shared.ViewModels.GameSystemVM
     {
+        private bool DependenciesOverride { get; set; }
+
         public ICore Core { get; private set; }
         public IEnumerable<string> MultiFileExtensions { get; private set; }
 
-        public GameSystemVM(ICore core, ILocalizationService localizer, string nameResKey, string manufacturerResKey, string symbol, IEnumerable<string> supportedExtensionsOverride = null, IEnumerable<string> multiFileExtensions = null) :
+        public GameSystemVM(ICore core, ILocalizationService localizer, string nameResKey, string manufacturerResKey, string symbol, bool dependenciesOverride = false, IEnumerable<string> supportedExtensionsOverride = null, IEnumerable<string> multiFileExtensions = null) :
             base(localizer, nameResKey, manufacturerResKey, symbol, supportedExtensionsOverride == null ? core.SupportedExtensions : supportedExtensionsOverride)
         {
+            DependenciesOverride = dependenciesOverride;
+
             Core = core;
             MultiFileExtensions = multiFileExtensions == null ? new string[0] : multiFileExtensions;
         }
@@ -29,6 +33,11 @@ namespace RetriX.UWP.ViewModels
 
         public override async Task<bool> CheckDependenciesMetAsync()
         {
+            if (DependenciesOverride)
+            {
+                return true;
+            }
+
             var systemFolder = Core.SystemFolder;
             foreach (var i in Core.FileDependencies)
             {
