9c2646fd780d98649503082bc4107fe43a1114e3
Alberto Fustinoni <alberto@aftnet.net>
2017-12-17 19:06:58 +0900

    Logic

--- LibretroRT.Test/Cores/VBAMCoreTest.cs
+++ LibretroRT.Test/Cores/VBAMCoreTest.cs
@@ -3,26 +3,26 @@ using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-//    public class VBAMCoreTest : TestBase
-//    {
-//        protected const string RomName = "GBAGame.gba";
+    public class VBAMCoreTest : TestBase
+    {
+        protected const string RomName = "GBAGame.gba";
 
-//        public VBAMCoreTest() : base(() => VBAMRT.VBAMCore.Instance)
-//        {
-//        }
+        public VBAMCoreTest() : base(() => VBAMRT.VBAMCore.Instance)
+        {
+        }
 
-//        [Theory]
-//        [InlineData(RomName)]
-//        public override Task LoadingRomWorks(string romName)
-//        {
-//            return LoadingRomWorksInternal(romName);
-//        }
+        [Theory]
+        [InlineData(RomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-//        [Theory]
-//        [InlineData(RomName)]
-//        public override Task ExecutionWorks(string romName)
-//        {
-//            return ExecutionWorksInternal(romName);
-//        }
-//    }
+        [Theory]
+        [InlineData(RomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
