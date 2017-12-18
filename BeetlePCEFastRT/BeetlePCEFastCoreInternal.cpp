3452c78f08950d451d0f640cd11f018ee4d3dd5d
Alberto Fustinoni <alberto@aftnet.net>
2017-12-18 20:53:53 +0900

    PC Engine

--- BeetlePCEFastRT/BeetlePCEFastCoreInternal.cpp
+++ BeetlePCEFastRT/BeetlePCEFastCoreInternal.cpp
@@ -1,12 +1,10 @@
 ﻿#include "pch.h"
 #include "BeetlePCEFastCoreInternal.h"
 
-#include "../LibretroRT/libretro.h"
-#include "../LibretroRT/libretro_extra.h"
-#include "../LibretroRT_Tools/Converter.h"
+#include "libretro.h"
 
 using namespace BeetlePCEFastRT;
-using namespace LibretroRT_Tools;
+using namespace LibretroRT_Shared;
 
 BeetlePCEFastCoreInternal^ coreInstance = nullptr;
 
@@ -15,24 +13,13 @@ BeetlePCEFastCoreInternal^ BeetlePCEFastCoreInternal::Instance::get()
 	if (coreInstance == nullptr)
 	{
 		coreInstance = ref new BeetlePCEFastCoreInternal();
-
-		retro_set_environment([](unsigned cmd, void* data) { return coreInstance->EnvironmentHandler(cmd, data); });
-		retro_set_input_poll([]() { coreInstance->RaisePollInput(); });
-		retro_set_input_state([](unsigned port, unsigned device, unsigned index, unsigned keyId) { return coreInstance->RaiseGetInputState(port, device, index, keyId); });
-		retro_set_audio_sample([](int16_t left, int16_t right) { coreInstance->SingleAudioFrameHandler(left, right); });
-		retro_set_audio_sample_batch([](const int16_t* data, size_t numFrames) { return coreInstance->RaiseRenderAudioFrames(data, numFrames); });
-		retro_set_video_refresh([](const void *data, unsigned width, unsigned height, size_t pitch) { coreInstance->RaiseRenderVideoFrame(data, width, height, pitch); });
-		retro_extra_set_open_file([](String^ filePath, FileAccessMode accessMode) { return coreInstance->OpenFileStream(filePath, accessMode); });
-		retro_extra_set_close_file([](IRandomAccessStream^ stream) { coreInstance->CloseFileStream(stream); });
+		CoreBase::SingletonInstance = coreInstance;
 	}
 
 	return coreInstance;
 }
 
-BeetlePCEFastCoreInternal::BeetlePCEFastCoreInternal() : LibretroRT_Tools::CoreBase(retro_init, retro_deinit,
-	retro_get_system_info, retro_get_system_av_info, retro_set_controller_port_device,
-	retro_load_game, retro_unload_game, retro_run, retro_reset, retro_serialize_size, retro_serialize, retro_unserialize,
-	true, true, false)
+BeetlePCEFastCoreInternal::BeetlePCEFastCoreInternal() : CoreBase(true, true, false)
 {
 	fileDependencies->Append(ref new FileDependency(L"syscard3.pce", L"PC Engine CD BIOS", L"ff1a674273fe3540ccef576376407d1d"));
 }
@@ -40,4 +27,5 @@ BeetlePCEFastCoreInternal::BeetlePCEFastCoreInternal() : LibretroRT_Tools::CoreB
 BeetlePCEFastCoreInternal::~BeetlePCEFastCoreInternal()
 {
 	coreInstance = nullptr;
+	CoreBase::SingletonInstance = nullptr;
 }
--- LibretroRT.Test/Cores/BeetlePCEFastCoreTest.cs
+++ LibretroRT.Test/Cores/BeetlePCEFastCoreTest.cs
@@ -3,29 +3,29 @@ using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-    //public class BeetlePCEFastCoreTest : TestBase
-    //{
-    //    protected const string PCERomName = "PCEngineGame.pce";
-    //    protected const string PCECDRomName = "PCEngineCDGame.cue";
+    public class BeetlePCEFastCoreTest : TestBase
+    {
+        protected const string PCERomName = "PCEngineGame.pce";
+        protected const string PCECDRomName = "PCEngineCDGame.cue";
 
-    //    public BeetlePCEFastCoreTest() : base(() => BeetleNGPRT.BeetleNGPCore.Instance)
-    //    {
-    //    }
+        public BeetlePCEFastCoreTest() : base(() => BeetleNGPRT.BeetleNGPCore.Instance)
+        {
+        }
 
-    //    [Theory]
-    //    [InlineData(PCERomName)]
-    //    [InlineData(PCECDRomName)]
-    //    public override Task LoadingRomWorks(string romName)
-    //    {
-    //        return LoadingRomWorksInternal(romName);
-    //    }
+        [Theory]
+        [InlineData(PCERomName)]
+        [InlineData(PCECDRomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-    //    [Theory]
-    //    [InlineData(PCERomName)]
-    //    [InlineData(PCECDRomName)]
-    //    public override Task ExecutionWorks(string romName)
-    //    {
-    //        return ExecutionWorksInternal(romName);
-    //    }
-    //}
+        [Theory]
+        [InlineData(PCERomName)]
+        [InlineData(PCECDRomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
