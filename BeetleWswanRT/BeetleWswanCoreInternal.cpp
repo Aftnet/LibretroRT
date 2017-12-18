2dcdb5471f0563beaa120dd536d4b4d76fd03eb1
Alberto Fustinoni <alberto@aftnet.net>
2017-12-18 20:31:29 +0900

    Wonderswan

--- BeetleWswanRT/BeetleWswanCoreInternal.cpp
+++ BeetleWswanRT/BeetleWswanCoreInternal.cpp
@@ -1,12 +1,10 @@
 ﻿#include "pch.h"
 #include "BeetleWswanCoreInternal.h"
 
-#include "../LibretroRT/libretro.h"
-#include "../LibretroRT/libretro_extra.h"
-#include "../LibretroRT_Tools/Converter.h"
+#include "libretro.h"
 
 using namespace BeetleWswanRT;
-using namespace LibretroRT_Tools;
+using namespace LibretroRT_Shared;
 
 BeetleWswanCoreInternal^ coreInstance = nullptr;
 
@@ -15,28 +13,18 @@ BeetleWswanCoreInternal^ BeetleWswanCoreInternal::Instance::get()
 	if (coreInstance == nullptr)
 	{
 		coreInstance = ref new BeetleWswanCoreInternal();
-
-		retro_set_environment([](unsigned cmd, void* data) { return coreInstance->EnvironmentHandler(cmd, data); });
-		retro_set_input_poll([]() { coreInstance->RaisePollInput(); });
-		retro_set_input_state([](unsigned port, unsigned device, unsigned index, unsigned keyId) { return coreInstance->RaiseGetInputState(port, device, index, keyId); });
-		retro_set_audio_sample([](int16_t left, int16_t right) { coreInstance->SingleAudioFrameHandler(left, right); });
-		retro_set_audio_sample_batch([](const int16_t* data, size_t numFrames) { return coreInstance->RaiseRenderAudioFrames(data, numFrames); });
-		retro_set_video_refresh([](const void *data, unsigned width, unsigned height, size_t pitch) { coreInstance->RaiseRenderVideoFrame(data, width, height, pitch); });
-		retro_extra_set_open_file([](String^ filePath, FileAccessMode accessMode) { return coreInstance->OpenFileStream(filePath, accessMode); });
-		retro_extra_set_close_file([](IRandomAccessStream^ stream) { coreInstance->CloseFileStream(stream); });
+		CoreBase::SingletonInstance = coreInstance;
 	}
 
 	return coreInstance;
 }
 
-BeetleWswanCoreInternal::BeetleWswanCoreInternal() : LibretroRT_Tools::CoreBase(retro_init, retro_deinit, 
-	retro_get_system_info, retro_get_system_av_info, retro_set_controller_port_device,
-	retro_load_game, retro_unload_game, retro_run, retro_reset, retro_serialize_size, retro_serialize, retro_unserialize,
-	true, true, false)
+BeetleWswanCoreInternal::BeetleWswanCoreInternal() : CoreBase(true, true, false)
 {
 }
 
 BeetleWswanCoreInternal::~BeetleWswanCoreInternal()
 {
 	coreInstance = nullptr;
+	CoreBase::SingletonInstance = nullptr;
 }
--- LibretroRT.Test/Cores/BeetleWswanCoreTest.cs
+++ LibretroRT.Test/Cores/BeetleWswanCoreTest.cs
@@ -3,26 +3,26 @@ using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-    //public class BeetleWswanCoreTest : TestBase
-    //{
-    //    protected const string RomName = "WonderSwanGame.wsc";
+    public class BeetleWswanCoreTest : TestBase
+    {
+        protected const string RomName = "WonderSwanGame.wsc";
 
-    //    public BeetleWswanCoreTest() : base(() => BeetleWswanRT.BeetleWswanCore.Instance)
-    //    {
-    //    }
+        public BeetleWswanCoreTest() : base(() => BeetleWswanRT.BeetleWswanCore.Instance)
+        {
+        }
 
-    //    [Theory]
-    //    [InlineData(RomName)]
-    //    public override Task LoadingRomWorks(string romName)
-    //    {
-    //        return LoadingRomWorksInternal(romName);
-    //    }
+        [Theory]
+        [InlineData(RomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-    //    [Theory]
-    //    [InlineData(RomName)]
-    //    public override Task ExecutionWorks(string romName)
-    //    {
-    //        return ExecutionWorksInternal(romName);
-    //    }
-    //}
+        [Theory]
+        [InlineData(RomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
