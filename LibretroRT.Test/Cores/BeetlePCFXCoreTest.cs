564f5793b147ea6c1557eb9bc0014fcab974ae11
Alberto Fustinoni <alberto@aftnet.net>
2017-12-18 22:05:43 +0900

    Logic

--- LibretroRT.Test/Cores/BeetlePCFXCoreTest.cs
+++ LibretroRT.Test/Cores/BeetlePCFXCoreTest.cs
@@ -3,26 +3,26 @@ using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-    //public class BeetlePCFXCoreTest : TestBase
-    //{
-    //    protected const string RomName = "PCFXGame.cue";
+    public class BeetlePCFXCoreTest : TestBase
+    {
+        protected const string RomName = "PCFXGame.cue";
 
-    //    public BeetlePCFXCoreTest() : base(() => BeetlePCFXRT.BeetlePCFXCore.Instance)
-    //    {
-    //    }
+        public BeetlePCFXCoreTest() : base(() => BeetlePCFXRT.BeetlePCFXCore.Instance)
+        {
+        }
 
-    //    [Theory]
-    //    [InlineData(RomName)]
-    //    public override Task LoadingRomWorks(string romName)
-    //    {
-    //        return LoadingRomWorksInternal(romName);
-    //    }
+        [Theory]
+        [InlineData(RomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-    //    [Theory]
-    //    [InlineData(RomName)]
-    //    public override Task ExecutionWorks(string romName)
-    //    {
-    //        return ExecutionWorksInternal(romName);
-    //    }
-    //}
+        [Theory]
+        [InlineData(RomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
