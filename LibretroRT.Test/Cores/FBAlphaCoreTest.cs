91d9041d5062c69673676f8834cbf4b9305a47cc
Alberto Fustinoni <alberto@aftnet.net>
2017-12-20 02:42:42 +0900

    Logic

--- LibretroRT.Test/Cores/FBAlphaCoreTest.cs
+++ LibretroRT.Test/Cores/FBAlphaCoreTest.cs
@@ -3,29 +3,29 @@ using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
-    //public class FBAlphaCoreTest : TestBase
-    //{
-    //    protected const string CPS1RomName = "3wondersu.zip";
-    //    protected const string NeoGeoRomName = "mslug.zip";
+    public class FBAlphaCoreTest : TestBase
+    {
+        protected const string CPS1RomName = "3wondersu.zip";
+        protected const string NeoGeoRomName = "mslug.zip";
 
-    //    public FBAlphaCoreTest() : base(() => FBAlphaRT.FBAlphaCore.Instance)
-    //    {
-    //    }
+        public FBAlphaCoreTest() : base(() => FBAlphaRT.FBAlphaCore.Instance)
+        {
+        }
 
-    //    [Theory]
-    //    [InlineData(CPS1RomName)]
-    //    [InlineData(NeoGeoRomName)]
-    //    public override Task LoadingRomWorks(string romName)
-    //    {
-    //        return LoadingRomWorksInternal(romName);
-    //    }
+        [Theory]
+        [InlineData(CPS1RomName)]
+        [InlineData(NeoGeoRomName)]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
 
-    //    [Theory]
-    //    [InlineData(CPS1RomName)]
-    //    [InlineData(NeoGeoRomName)]
-    //    public override Task ExecutionWorks(string romName)
-    //    {
-    //        return ExecutionWorksInternal(romName);
-    //    }
-    //}
+        [Theory]
+        [InlineData(CPS1RomName)]
+        [InlineData(NeoGeoRomName)]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
 }
