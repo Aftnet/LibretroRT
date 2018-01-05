e4ecdb089fbdd5146c3dd3409cffe08ede33c674
Alberto Fustinoni <alberto@aftnet.net>
2018-01-05 20:45:48 +0900

    Tests

--- LibretroRT.Test/Cores/BeetlePCFXCoreTest.cs
+++ LibretroRT.Test/Cores/BeetlePCFXCoreTest.cs
@@ -1,25 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace LibretroRT.Test.Cores
 {
     public class BeetlePCFXCoreTest : TestBase
     {
-        protected const string RomName = "PCFXGame.chd";
+        public static IEnumerable<object[]> RomNames => new List<object[]>
+        {
+            new object[] { "PCFXGame.chd" },
+        };
 
         public BeetlePCFXCoreTest() : base(() => BeetlePCFXRT.BeetlePCFXCore.Instance)
         {
         }
 
         [Theory]
-        [InlineData(RomName)]
+        [MemberData(nameof(RomNames))]
         public override Task LoadingRomWorks(string romName)
         {
             return LoadingRomWorksInternal(romName);
         }
 
         [Theory]
-        [InlineData(RomName)]
+        [MemberData(nameof(RomNames))]
         public override Task ExecutionWorks(string romName)
         {
             return ExecutionWorksInternal(romName);
