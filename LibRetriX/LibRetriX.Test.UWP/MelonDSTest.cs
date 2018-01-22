db5f1c17afe215b1edff7f3ef1f684bee6150bc3
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-01-22 10:58:22 +0900

    Tests

---
+++ LibRetriX/LibRetriX.Test.UWP/MelonDSTest.cs
@@ -0,0 +1,34 @@
+﻿using LibRetriX.GPGX;
+using LibRetriX.Test.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibRetriX.Test.UWP
+{
+    public class MelonDSTest : CoreTestBase
+    {
+        public static IEnumerable<object[]> RomNames => new List<object[]>
+        {
+            new object[] { "NDSGame.nds" },
+        };
+
+        public MelonDSTest() : base(() => Core.Instance)
+        {
+        }
+
+        [Theory]
+        [MemberData(nameof(RomNames))]
+        public override Task LoadingRomWorks(string romName)
+        {
+            return LoadingRomWorksInternal(romName);
+        }
+
+        [Theory]
+        [MemberData(nameof(RomNames))]
+        public override Task ExecutionWorks(string romName)
+        {
+            return ExecutionWorksInternal(romName);
+        }
+    }
+}
