6b5e428bd5f9856f565750ec8fe31ff3b360bfd8
Alberto Fustinoni <alberto@aftnet.net>
2018-01-22 17:35:11 +0900

    FIx

--- FCEUMM.LibRetriX/Core.cs
+++ FCEUMM.LibRetriX/Core.cs
@@ -12,16 +12,9 @@ namespace LibRetriX.FCEUMM
 
         private static ICore InitCore()
         {
-            var core = new LibretroCore(Dependencies);
+            var core = new LibretroCore();
             core.Initialize();
             return core;
         }
-
-        private static readonly FileDependency[] Dependencies =
-        {
-            new FileDependency("BIOS_CD_E.bin", "Mega-CD (Model 1 1.00 Europe) BIOS", "e66fa1dc5820d254611fdcdba0662372"),
-            new FileDependency("BIOS_CD_J.bin", "Mega-CD (Model 1 1.00 Japan) BIOS", "278a9397d192149e84e820ac621a8edd"),
-            new FileDependency("BIOS_CD_U.bin", "Mega-CD (Model 1 1.00 USA) BIOS", "2efd74e3232ff260e371b99f84024f7f"),
-        };
     }
 }
