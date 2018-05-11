c5bc14cec1fda53c4ef2cf6a1c3e342849e676af
Alberto Fustinoni <alberto@aftnet.net>
2018-05-11 22:49:38 +0900

    Logic

--- RetriX.UWP.Unsafe/Components/FramebufferConverter.cs
+++ RetriX.UWP.Unsafe/Components/FramebufferConverter.cs
@@ -8,6 +8,10 @@ namespace RetriX.UWP.Components
     {
         private const uint LookupTableSize = ushort.MaxValue + 1;
 
+        private static readonly uint[] RGB0555LookupTable = new uint[LookupTableSize];
+        private static readonly GCHandle RGB0555LookupTableHandle;
+        private static readonly uint* RGB0555LookupTablePtr;
+
         private static readonly uint[] RGB565LookupTable = new uint[LookupTableSize];
         private static readonly GCHandle RGB565LookupTableHandle;
         private static readonly uint* RGB565LookupTablePtr;
@@ -28,8 +32,34 @@ namespace RetriX.UWP.Components
                 RGB565LookupTable[i] = 0xFF000000 | r << 16 | g << 8 | b;
             }
 
+            for (uint i = 0; i < LookupTableSize; i++)
+            {
+                r = (i >> 10) & 0x1F;
+                g = (i >> 5) & 0x1F;
+                b = (i & 0x1F);
+
+                r = (uint)Math.Round(r * 255.0 / 31.0);
+                g = (uint)Math.Round(g * 255.0 / 31.0);
+                b = (uint)Math.Round(b * 255.0 / 31.0);
+
+                RGB0555LookupTable[i] = 0xFF000000 | r << 16 | g << 8 | b;
+            }
+
+            RGB0555LookupTableHandle = GCHandle.Alloc(RGB0555LookupTable, GCHandleType.Pinned);
+            RGB0555LookupTablePtr = (uint*)RGB0555LookupTableHandle.AddrOfPinnedObject();
+
             RGB565LookupTableHandle = GCHandle.Alloc(RGB565LookupTable, GCHandleType.Pinned);
             RGB565LookupTablePtr = (uint*)RGB565LookupTableHandle.AddrOfPinnedObject();
+        }
+
+        public static void ConvertFrameBufferRGB0555ToXRGB8888(uint width, uint height, IReadOnlyList<ushort> input, int inputPitch, byte* output, int outputPitch)
+        {
+            ConvertFrameBufferUshortToXRGB8888WithLUT(width, height, input, inputPitch, output, outputPitch, RGB0555LookupTablePtr);
+        }
+
+        public static void ConvertFrameBufferRGB565ToXRGB8888(uint width, uint height, IReadOnlyList<ushort> input, int inputPitch, byte* output, int outputPitch)
+        {
+            ConvertFrameBufferUshortToXRGB8888WithLUT(width, height, input, inputPitch, output, outputPitch, RGB565LookupTablePtr);
         }
 
         public static void ConvertFrameBufferXRGB8888(uint width, uint height, IReadOnlyList<uint> input, int inputPitch, byte* output, int outputPitch)
@@ -48,7 +78,7 @@ namespace RetriX.UWP.Components
             }
         }
 
-        public static void ConvertFrameBufferRGB565ToXRGB8888(uint width, uint height, IReadOnlyList<ushort> input, int inputPitch, byte* output, int outputPitch)
+        private static void ConvertFrameBufferUshortToXRGB8888WithLUT(uint width, uint height, IReadOnlyList<ushort> input, int inputPitch, byte* output, int outputPitch, uint* lutPtr)
         {
             var inLineStartIx = 0;
             for (var i = 0; i < height; i++)
@@ -56,17 +86,12 @@ namespace RetriX.UWP.Components
                 var outLineStart = (uint*)output;
                 for (var j = 0; j < width; j++)
                 {
-                    outLineStart[j] = RGB565LookupTablePtr[input[inLineStartIx + j]];
+                    outLineStart[j] = lutPtr[input[inLineStartIx + j]];
                 }
 
                 inLineStartIx += inputPitch;
                 output += outputPitch;
             }
-        }
-
-        public static void ConvertFrameBufferRGB0555ToXRGB8888(uint width, uint height, IReadOnlyList<ushort> input, int inputPitch, byte* output, int outputPitch)
-        {
-            throw new NotImplementedException();
         }
     }
 }
