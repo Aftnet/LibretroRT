019f8fdba212a0f4d489371ef3fd9ff243fc9f39
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-05-11 18:21:34 +0900

    Logic

---
+++ LibRetriX.Unsafe/UnmanagedListInt.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LibRetriX.RetroBindings.Unsafe
+{
+    public unsafe class UnmanagedListInt : IReadOnlyList<int>
+    {
+        private class InnerEnumerator : IEnumerator<int>
+        {
+            private int* StartPtr { get; }
+            private int* EndPtr { get; }
+            private int* CurrentPtr { get; set; } = null;
+
+            public int Current => *CurrentPtr;
+            object IEnumerator.Current => Current;
+
+            public InnerEnumerator(int* startPtr, int* endPtr)
+            {
+                StartPtr = startPtr;
+                EndPtr = endPtr;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                CurrentPtr++;
+                return CurrentPtr < EndPtr;
+            }
+
+            public void Reset()
+            {
+                CurrentPtr = StartPtr;
+            }
+        }
+
+        private int* StartPtr { get; }
+
+        public int Count { get; }
+        public int this[int index] => StartPtr[index];
+
+        public UnmanagedListInt(IntPtr startPtr, int count)
+        {
+            StartPtr = (int*)startPtr.ToPointer();
+            Count = count;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new InnerEnumerator(StartPtr, StartPtr + Count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LibRetriX.Unsafe/UnmanagedListShort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LibRetriX.RetroBindings.Unsafe
+{
+    public unsafe class UnmanagedListShort : IReadOnlyList<short>
+    {
+        private class InnerEnumerator : IEnumerator<short>
+        {
+            private short* StartPtr { get; }
+            private short* EndPtr { get; }
+            private short* CurrentPtr { get; set; } = null;
+
+            public short Current => *CurrentPtr;
+            object IEnumerator.Current => Current;
+
+            public InnerEnumerator(short* startPtr, short* endPtr)
+            {
+                StartPtr = startPtr;
+                EndPtr = endPtr;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                CurrentPtr++;
+                return CurrentPtr < EndPtr;
+            }
+
+            public void Reset()
+            {
+                CurrentPtr = StartPtr;
+            }
+        }
+
+        private short* StartPtr { get; }
+
+        public int Count { get; }
+        public short this[int index] => StartPtr[index];
+
+        public UnmanagedListShort(IntPtr startPtr, int count)
+        {
+            StartPtr = (short*)startPtr.ToPointer();
+            Count = count;
+        }
+
+        public IEnumerator<short> GetEnumerator()
+        {
+            return new InnerEnumerator(StartPtr, StartPtr + Count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
